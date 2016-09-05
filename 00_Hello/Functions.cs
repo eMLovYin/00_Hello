f48a00b60bdddda18def377a1b4048b0ab3d2114
Sez <Sez@LENOVO-PC>
2016-09-05 11:11:07 +0200

    Add project files.

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,14 @@
+﻿namespace _00_Hello
+{
+    internal class Functions
+    {
+        public static string Hello()
+        {
+            return "hello";
+        }
+        public static string HelloFred()
+        {
+            return "hello, Fred";
+        }
+    }
+}
